a02491986244011088456b4e18f91050016c9fbe
Chris Tippett <c.tippett@gmail.com>
2018-08-04 23:08:21 +1000

    Add basic routes for frontend app

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import RegisterError from '@/components/RegisterError'
+import RegisterConfig from '@/components/RegisterConfig'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/register/error',
+      name: 'RegisterError',
+      component: RegisterError
+    },
+    {
+      path: '/register/config',
+      name: 'RegisterConfig',
+      component: RegisterConfig
     }
   ]
 })
