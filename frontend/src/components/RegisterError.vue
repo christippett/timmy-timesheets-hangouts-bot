c014c6169e4336c6e7d83e1a93994fc185977c62
Chris Tippett <c.tippett@gmail.com>
2018-08-05 07:43:40 +1000

    Create error page for OAuth flow

--- frontend/src/components/RegisterError.vue
+++ frontend/src/components/RegisterError.vue
@@ -1,40 +1,29 @@
 <template>
-  <div class="error">
-    <h1>Error!</h1>
-    <h2>{{ errorDescription }}</h2>
-    <ul>
-      <li>
-        <a href="/">Home</a>
-      </li>
-    </ul>
+  <div class="container has-text-centered">
+    <div class="columns is-vcentered">
+      <div class="column is-5">
+        <figure class="image is-square">
+          <img src="../assets/timmy.png" alt="Description">
+        </figure>
+      </div>
+      <div class="column is-6 is-offset-1">
+        <h1 class="title is-2">
+          Authentication Error
+        </h1>
+        <h2 class="subtitle is-4">
+          We had trouble logging you in.
+        </h2>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'RegisterError',
-  data () {
-    return {
-      errorDescription: 'Could not authenticate user.'
-    }
-  }
+  name: 'RegisterError'
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
